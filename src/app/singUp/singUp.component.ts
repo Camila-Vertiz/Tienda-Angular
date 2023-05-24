4805cadc72793df9cf89858b668debbff4492f7f
Edson Contreras <edson.contreras113@gmail.com>
2023-05-24 14:12:31 -0500

    html de carrito, login y sing up

---
+++ src/app/singUp/singUp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singUp',
+  templateUrl: './singUp.component.html',
+  styleUrls: ['./singUp.component.scss']
+})
+export class SingUpComponent {
+
+}
