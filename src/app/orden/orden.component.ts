1feca447b6475cab8b18dbf6cba0d738fe7b4536
Camila-Vertiz <camila.vrtz@gmail.com>
2023-05-29 12:44:08 -0500

    fetch cliente en orden

--- src/app/orden/orden.component.ts
+++ src/app/orden/orden.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServiciosapiService } from '../servicio-api.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orden',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./orden.component.scss']
 })
 export class OrdenComponent {
+
+  cliente: any;
+
+  constructor(
+    private router: Router,
+    private apiService: ServiciosapiService
+  ) {
+    this.buscarUser();
+  }
+
+  buscarUser() {
+    const id_user = localStorage.getItem('id_user');
+    this.apiService.buscarUsuarioPorId(id_user)
+      .then(data => {
+        console.log(JSON.stringify(data));
+        const jsonRespuesta = JSON.stringify(data);
+        const Respuesta = JSON.parse(jsonRespuesta);
+        this.cliente = Respuesta[0];
+        console.log(this.cliente);
+      })
+      .catch(error => {
+        //console.log(error);
+      });
+  }
 
 }
