78a9c3f958f6270098f0d29690131b6add700761
Camila-Vertiz <camila.vrtz@gmail.com>
2023-05-29 12:22:59 -0500

    calcular subtotal

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -8,36 +8,38 @@ import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
   styleUrls: ['./carrito.component.scss']
 })
 export class CarritoComponent {
-  idproducto:any;
-  nombre:any;
-  precio:any;
-  cantidad:any;
-  total:any;
-  carrito:any[];
+  total: any;
+  carrito: any[];
 
   constructor(
     private apiService: ServiciosapiService,
     public router: Router,
     private route: ActivatedRoute) {
+
+    this.buscarCarritoPorCliente();
   }
 
-  ngOnInit() {
+  buscarCarritoPorCliente() {
     const id_usuario = localStorage.getItem('id_user');
-   
     this.apiService.buscarCarritoPorCliente(id_usuario)
       .then(data => {
         const jsonRespuesta = JSON.stringify(data);
         const Respuesta = JSON.parse(jsonRespuesta);
-       this.carrito = Respuesta;
-        console.log( this.carrito);
+        this.carrito = Respuesta;
+        this.asc();
+        this.calcularTotal();
       })
       .catch(error => {
         console.log(error);
       });
-    this.asc();
-    console.log(this.carrito)
   }
+
+
   asc() {
     this.carrito.sort((a, b) => a.id_carrito - b.id_carrito);
+  }
+
+  calcularTotal() {
+    this.total= this.carrito.reduce((accumulator, element) => accumulator + element.total, 0);
   }
 }
