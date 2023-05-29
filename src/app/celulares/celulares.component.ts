9ff322e0fb7da3ca81e9764d1f255c277bee261b
Camila-Vertiz <camila.vrtz@gmail.com>
2023-05-28 23:17:40 -0500

    mostrar celulares

--- src/app/celulares/celulares.component.ts
+++ src/app/celulares/celulares.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiciosapiService } from '../servicio-api.component';
 
 @Component({
   selector: 'app-celulares',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./celulares.component.scss']
 })
 export class CelularesComponent {
+  productos: any[] = [];
+  terminoBusqueda = "";
 
+  constructor(
+    private apiService: ServiciosapiService) { }
+
+  ngOnInit() {
+    this.apiService.listarProductos().subscribe(data => {
+      this.productos = data.filter(producto => producto.id_categoria === 1);
+    });
+    this.productos.sort((a, b) => a.id - b.id);
+  }
+  getProductosFiltrados(): any[] {
+    if (!this.terminoBusqueda) {
+      return this.productos;
+    }
+    const termino = this.terminoBusqueda.toLowerCase();
+    return this.productos.filter((producto: any) => {
+
+      const nombres = `${producto.nombre}`.toLowerCase();
+      return nombres.includes(termino);
+    });
+  }
 }
